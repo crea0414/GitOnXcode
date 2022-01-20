352178c53398ef981a643255370882c777aa1d8d
creacyhuang <creacyhuang@gmail.com>
2022-01-20 17:58:30 +0800

    Initial Commit

---
+++ GitOnXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitOnXcode
+//
+//  Created by 吳佳穎 on 2022/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
